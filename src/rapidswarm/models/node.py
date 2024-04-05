bd1ea83dfb1f1b3ef7ef601e2e2ef99dcdb1ba33
Lorenzo Swank <lswank@gmail.com>
2024-04-04 23:09:48 -0600

    Added switch concept and tests

--- src/rapidswarm/models/node.py
+++ src/rapidswarm/models/node.py
@@ -1,15 +1,21 @@
-from typing import List
-
+from typing import List, Optional
 from pydantic import BaseModel, Field
 
 from .gpu import GPU
 from .network_interface import NetworkInterface
 
+class NetworkSwitch(BaseModel):
+    id: str = Field(..., description="Unique identifier for the network switch")
+    model: str = Field(..., description="Model of the network switch")
+    ip_address: str = Field(..., description="IP address of the network switch")
 
 class Node(BaseModel):
-    id: str = Field(..., description="Unique identifier for the node")
+    id: Optional[str] = Field(None, description="Unique identifier for the node")
     hostname: str = Field(..., description="Hostname of the node")
     network_interfaces: List[NetworkInterface] = Field(
         [], description="List of network interfaces associated with the node"
     )
     gpus: List[GPU] = Field([], description="List of GPUs associated with the node")
+    network_switch: Optional[NetworkSwitch] = Field(
+        None, description="Associated network switch for the node"
+    )
